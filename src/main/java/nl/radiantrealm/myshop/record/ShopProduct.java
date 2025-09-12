e520ec0e954a5f9eea4e39215163beffdff8d751
Josje <paul.verbael@gmail.com>
2025-09-12 23:47:52 +0200

    Refactor ShopProduct materials to use String keys
    Changed the materials map in ShopProduct from Map<Material, Integer> to Map<String, Integer> for improved JSON serialization and simplified code. Removed unused utility methods and updated toJson to reflect the new structure.

--- src/main/java/nl/radiantrealm/myshop/record/ShopProduct.java
+++ src/main/java/nl/radiantrealm/myshop/record/ShopProduct.java
@@ -1,47 +1,29 @@
 package nl.radiantrealm.myshop.record;
 
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import nl.radiantrealm.library.utils.DataObject;
-import nl.radiantrealm.library.utils.FormatUtils;
 import org.bukkit.Material;
 
-import java.util.HashMap;
 import java.util.Map;
 
-public record ShopProduct(String productKey, String productName, Material productIcon, String productCategory, Map<Material, Integer> materials) implements DataObject {
+public record ShopProduct(String productKey, String productName, Material productIcon, String productCategory, Map<String, Integer> materials) implements DataObject {
 
     @Override
     public JsonObject toJson() throws IllegalStateException {
-        JsonObject object = new JsonObject();
-        object.addProperty("product_key", productKey);
-        object.addProperty("product_name", productName);
-        object.addProperty("product_icon", productIcon.name());
-        object.addProperty("product_category", productCategory);
-        object.add("materials", parseMaterialJSON(materials));
-        return object;
-    }
+        JsonObject result = new JsonObject();
+        result.addProperty("product_key", productKey);
+        result.addProperty("product_name", productName);
+        result.addProperty("product_icon", productIcon.name());
+        result.addProperty("product_category", productCategory);
 
-    public static Map<Material, Integer> parseMaterialMap(JsonObject object) throws IllegalArgumentException {
-        Map<Material, Integer> map = new HashMap<>(object.size());
-
-        for (Map.Entry<String, JsonElement> entry : object.entrySet()) {
-            map.put(
-                    FormatUtils.formatEnum(Material.class, entry.getKey()),
-                    FormatUtils.formatInteger(entry.getValue().getAsString())
-            );
-        }
-
-        return map;
-    }
-
-    public static JsonObject parseMaterialJSON(Map<Material, Integer> materials) throws IllegalStateException {
         JsonObject object = new JsonObject();
 
-        for (Map.Entry<Material, Integer> entry : materials.entrySet()) {
-            object.addProperty(entry.getKey().name(), entry.getValue());
+        for (Map.Entry<String, Integer> entry : materials.entrySet()) {
+            object.addProperty(entry.getKey(), entry.getValue());
         }
 
-        return object;
+        result.add("materials", object);
+
+        return result;
     }
 }
